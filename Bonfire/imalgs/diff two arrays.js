855d49cbe4aa1769994ea327c8721c687ad40dda
idietmoran <idietmoran@gmail.com>
2015-11-16 01:17:24 -0500

    diff two arrays

--- Bonfire/imalgs/diff two arrays.js
+++ Bonfire/imalgs/diff two arrays.js
@@ -1,7 +1,11 @@
 function diff(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
-  return newArr;
+    var newArr = [];
+    
+    return newArr;
+    
+//    var remove = Array.prototype.slice.call(arguments);
+//    return arr1.filter(function(element) {
+//    });
 }
 
-diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diff([1, 2, 3, 5], [1, 2, 3, 4, 5]));
