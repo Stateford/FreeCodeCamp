a614d945628891134241f57f865fa7dc3d7e0abe
idiet moran <idietmoran@gmail.com>
2015-12-10 09:57:15 -0500

    1

--- Bonfire/imalgs/Sum All Primes.js
+++ Bonfire/imalgs/Sum All Primes.js
@@ -3,18 +3,10 @@ function sumPrimes(num) {
     var total = 0;
     
     for(var i = 0; i < num; i++) {
-        if(num % i === 0) {
-            prime.push(i);
-        }
+        
     }
     
-    for(var x = 0; x < prime.length; x++) {
-        total += prime[i];
-    }
-    console.log(total);
-    console.log(prime);
-    return total;
-}
+};
 
 console.log(sumPrimes(10));
 console.log(sumPrimes(19));
