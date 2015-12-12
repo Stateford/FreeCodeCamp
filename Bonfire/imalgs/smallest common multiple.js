48fecef91258778a7ee79dde9a8f97b9ee4c4210
idiet moran <idietmoran@gmail.com>
2015-12-12 11:49:37 -0500

    bonfire

---
+++ Bonfire/imalgs/smallest common multiple.js
@@ -0,0 +1,11 @@
+function smallestCommons(arr) {
+    arr.sort();
+    
+    for(var i = 0; i < arr.length; i++) {
+        arr[i]
+    }
+    return arr;
+}
+
+
+console.log(smallestCommons([1,5]));
