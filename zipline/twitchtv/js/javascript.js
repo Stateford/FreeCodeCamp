ea472c78e6507f52d8f511f503eed3f66ca06684
idietmoran <idietmoran@gmail.com>
2016-01-24 18:29:59 -0500

    1

--- zipline/twitchtv/js/javascript.js
+++ zipline/twitchtv/js/javascript.js
@@ -5,3 +5,28 @@ var streamList = ['arteezy', 'eternalenvy', 'feardarkness', 'd3xt3r'];
 $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
   console.log(data);
 });
+
+$(document).ready(function() {
+    
+    //ONLINE TAB
+    $('.online').click(function() {
+        $('.offline').removeClass('active');
+        $('.all').removeClass('active');
+        $('.online').addClass('active');
+    });
+    
+    
+    //OFFLINE TAB
+    $('.offline').click(function() {
+        $('.offline').addClass('active');
+        $('.online').removeClass('active');
+        $('.all').removeClass('active');
+    });
+    
+    //ALL TAB
+    $('.all').click(function() {
+        $('.offline').removeClass('active');
+        $('.all').addClass('active');
+        $('online').removeClass('active');
+    });
+});
