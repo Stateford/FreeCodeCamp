f4ed80c1918c91e1253315164801bdfbc7e5b95e
idietmoran <idietmoran@gmail.com>
2016-01-25 18:54:20 -0500

    1

--- zipline/twitchtv/js/controllers/MainController.js
+++ zipline/twitchtv/js/controllers/MainController.js
@@ -1,9 +1,26 @@
 app.controller('MainController', function($scope, $http) {
+    
+    var streamList = ['arteezy', 'cerebralwallsy', 'feardarkness', 'blackdotatv'];
+
+    $scope.username = {};
+    
+    for(var i = 0; i < streamList.length; i++) {
+        $scope.streamList[i] = {};
+        $http.get("https://api.twitch.tv/kraken/streams" + streamList[i])
+        .success(function(data) {
+            $scope.username.streamList[i] = data;
+            console.log($scope.data)
+        })
+        .error(function(data) {
+            console.log('got nothing');
+        });
+    };
+    
     $scope.streams = {};
     $http.get("https://api.twitch.tv/kraken/streams/blackdotatv")
     .success(function(data) {
         $scope.streams = data;
-        console.log($scope.results);
+        console.log($scope.data);
     })
     .error(function(data) {
         console.log('got nothing');
