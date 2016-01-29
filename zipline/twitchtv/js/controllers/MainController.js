1ea5e3dbf9191b8d6860eb96a2c3f8b78ebe81cf
idiet moran <idietmoran@gmail.com>
2016-01-28 21:11:00 -0500

    7

--- zipline/twitchtv/js/controllers/MainController.js
+++ zipline/twitchtv/js/controllers/MainController.js
@@ -1,8 +1,7 @@
 app.controller('MainController', function($scope, $http) {
 
     
-    var usernames = ['arteezy', 'feardarkness', 'cerebralwallsy', 'summit1g', 'pianoimproman', 'wagamamatv', "monstercat", "blackdotatv", "illidanstrdoto"];
-    var offlineusers = [];
+    var usernames = ["arteezy", "feardarkness", "cerebralwallsy", "summit1g", "pianoimproman", "wagamamatv", "monstercat", "blackdotatv", "illidanstrdoto"];
     
     $scope.online = [];
     $scope.offline = [];
@@ -18,11 +17,15 @@ app.controller('MainController', function($scope, $http) {
                 $scope.online.push(data);
             }
             else if(data.stream === undefined || data.stream === null) {
-                $scope.offline.push(usernames[i]);
-                console.log(usernames[i]);
+                console.log('offline');
+                console.log(data);
+                $scope.offline.push(current);
+                console.log('username');
+                console.log(current);
                 console.log($scope.offline);
             }
             $scope.all.push(data);
+            console.log($scope.all);
         })
         .error(function(data) {
             console.log('got nothing');
---
+++ zipline/wikiviewer/js/controller/WikiCtrl.js
@@ -0,0 +1,3 @@
+app.controller('WikiCtrl', function($scope, $http) {
+    
+});
