e9235ad326bfb7b36f7e324d54b8eff107dc45ae
idiet moran <idietmoran@gmail.com>
2016-02-04 03:26:01 -0500

    1

--- zipline/wikiviewer/js/controller/WikiCtrl.js
+++ zipline/wikiviewer/js/controller/WikiCtrl.js
@@ -1,11 +1,26 @@
 app.controller('WikiCtrl', function($scope, $http) {
     
+    
+    
     $scope.GetQuery = function() {
-        getSearchBox = $('.searchbox').val();
-        $http.get('' + getSearchBox)
+        $scope.name = [];
+        $scope.description = [];
+        $scope.url = [];
+        
+        getSearchBox = $('#searchbox-default').val();
+        $http.jsonp('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=JSON_CALLBACK&search=' + getSearchBox)
         .success(function(data) {
             $scope.results = data;
-        })
+            
+            for(var i = 0; i < data[1].length; i++) {
+                $scope.name.push(data[1][i]);
+                $scope.description.push(data[2][i]);
+                $scope.url.push(data[3][i]);
+            };
+            
+            
+            console.log(data);
+        });
         
     }
 });
