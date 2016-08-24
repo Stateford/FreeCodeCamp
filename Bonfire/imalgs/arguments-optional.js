1a7a66137f67d1e989fef3fdfaff333e6326d90d
idietmoran <idietmoran@gmail.com>
2016-08-24 03:32:37 -0400

    1

--- Bonfire/imalgs/arguments-optional.js
+++ Bonfire/imalgs/arguments-optional.js
@@ -1,5 +1,41 @@
-function add() {
-    return false;
+function addTogether(...arg) {
+    let total = 0;
+    if(arg.length >= 2) {
+        for(let num of arg) {
+            if(typeof num === 'number') {
+                total += num;
+            } else if(typeof num !== 'number') {
+                return undefined;
+            }
+        }
+        return total;
+    } else if(typeof arg[0] !== 'number') {
+        return undefined;
+    } else {
+        try {
+            for(let num of arg) {
+                if(typeof num !== "number") {
+                    return "undefined";
+                } else {
+                    total += num;
+                }
+            }
+            return function(c) {
+                if(typeof c !== 'number') {
+                    return undefined;
+                } else {
+                    total += c;
+                    return total;
+                }
+            };
+        } catch(e) {
+            throw e;
+        }
+        return total;
+    }
+    return total;
 }
 
-add(2,3);
+console.log(addTogether(2, 3));
+console.log(addTogether(2)(3));
+console.log(addTogether("http://bit.ly/IqT6zt"));
